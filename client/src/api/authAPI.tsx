c2c845fc02ed2557097baf3e58fea1736fa41ff1
Andrew Birmingham <andrew_birmingham@msn.com>
2025-02-27 15:02:52 -0700

    adding some notes

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -3,6 +3,7 @@ import { UserLogin } from "../interfaces/UserLogin";
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
 try {
+  //response fetches login info and stringifies body containing the userInfo in JSON 
   const response = await fetch('/auth/login', {
     method: 'POST',
     headers: {
@@ -13,15 +14,16 @@ try {
   });
 
   const data = await response.json();
-
+//Response is not ok due to no user info
   if (!response.ok) {
-    throw new Error('User information not retrieved, check network tab!');
+    throw new Error('User info not available. Check network tab!');
   }
 
   return data;
+  //error when trying to get user login import
 } catch (err) {
   console.log('Error from user login: ', err);
-  return Promise.reject('Could not fetch user info');
+  return Promise.reject('Could not fetch the user info');
 }
 };
 
